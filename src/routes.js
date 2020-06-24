e5575a43e29f82895caacf93553a8606f1296c95
pedrofuzz <pedrofuzario@hotmail.com>
2020-06-24 09:28:05 -0400

    adiciona rota thumbnail

--- src/routes.js
+++ src/routes.js
@@ -15,10 +15,6 @@ routes.get("/projects/:project_id", ProjectController.show);
 
 routes.post("/admin/login", AuthController.login);
 
-routes.get("/files/:name", (res, req) => {
-  res.status(200);
-});
-
 routes.use(authMiddleware).get("/auth", AuthController.authenticated);
 
 routes
